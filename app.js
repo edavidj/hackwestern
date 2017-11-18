46df9b85abb87cb1cbe6a487964fab9a3d53eda8
Ethan Johnston <johnse20@mcmaster.ca>
2017-11-18 15:51:28 -0500

    changd landing page set up query for user untested

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var express     = require("express"),
     passport    = require("passport"),
     localStrategy = require("passport-local"),
     config      = require("./config"),
-    User        = require("./models/User"),
     users       = require("./models/Users"),
     businesses  = require("./models/Businesses"),
     reviews     = require("./models/Reviews"),
@@ -44,23 +43,17 @@ app.get('/reviews', function(req, res){
         res.send(reviews); 
     });
 });
-app.use(flash());
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended:true})); //parse form and query variables better
-app.set("view engine", "ejs");
-// ========= PASSPORT INIT ============
 app.use(require("express-session")({
     secret: "i want chips",
     resave: false,
     saveUninitialized: false
 }));
-app.use(passport.initialize());
-app.use(passport.session()); 
-passport.use(new localStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+app.use(flash());
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true})); //parse form and query variables better
+app.set("view engine", "ejs");
+
 app.use(function(req,res,next){
-    res.locals.user = req.user;
     res.locals.error = req.flash("error");
     res.locals.success = req.flash("success");
     next();
@@ -70,41 +63,12 @@ app.use(function(req,res,next){
 app.get("/", function(req,res){
     res.render("landing");
 });
-//======= AUTH ========
-app.get("/login", function(req,res){
-    res.render("login");
-});
-app.post("/login", passport.authenticate("local", {
-    // successRedirect: "/user/",
-    // successFlash: "Welcome back!",
-    failureRedirect: "/login",
-    failureFlash: "Incorrect login credentials."
-}), function(req,res){
-    //do nothing for now
-    req.flash("success", "Welcome back, "+req.user.username);
-    res.redirect("/user/"+req.user.username);
-});
-app.get("/register", function(req,res){
-    res.render("register");
-});
-app.post("/register", function(req,res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            req.flash("error", err.message); 
-            res.redirect("back");
-            return;
-        }
-        passport.authenticate("local")(req,res,function(){
-            req.flash("success", "Welcome, "+user.username);
-            console.log("Success");
-            res.redirect("/");
-        });
-    });
-});
-app.get("/logout", function(req,res){
-    req.logout();
-    res.redirect("/");
+app.get("/search", function(req,res){
+    users.findById(req.body.username, function(err, user){
+        if(err) throw err;
+        console.log(user);
+        res.render("account", {user: user});
+    })
 });
 //======= USER ROUTES =========
 app.get("/user/:id", isLoggedIn, function(req,res){
