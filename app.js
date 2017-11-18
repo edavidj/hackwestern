3c0905378dc7e870149dc77ba3825238a3d03d9e
Ethan Johnston <johnse20@mcmaster.ca>
2017-11-18 18:26:14 -0500

    attempt at fix for search crash

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.get("/", function(req,res){
 });
 app.post("/search", function(req,res){
     users.find({name: req.body.username}, function(err, user){
-        if(err){
+        if(err || user === null){
             console.log(err);
             return;
         }
