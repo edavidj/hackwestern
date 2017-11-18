40822784f66ef38d794f212db6a7efa94d0e6506
Ethan Johnston <johnse20@mcmaster.ca>
2017-11-18 04:08:36 -0500

    user association added, no account route yet.

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express     = require("express"),
     path        = require("path"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    localStrategy = require("passport-local"),
     config      = require("./config"),
+    User        = require("./models/User"),
     app         = express();
 
 mongoose.connect(config.db, {useMongoClient:true});
-
+// ==== mongo example ====
 // var testSchema = mongoose.Schema({ //define schema properties
 //     text: String
 // });
@@ -21,6 +24,21 @@ mongoose.connect(config.db, {useMongoClient:true});
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true})); //parse form and query variables better
 app.set("view engine", "ejs");
+// ========= PASSPORT INIT ============
+app.use(require("express-session")({
+    secret: "i want chips",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req,res,next){
+    res.locals.user = req.user;
+    next();
+});   
 //for ref to front end files /<folder>/<file>
 //RENDER ROUTES
 app.get("/", function(req,res){
@@ -30,8 +48,31 @@ app.get("/", function(req,res){
 app.get("/login", function(req,res){
     res.render("login");
 });
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    // successFlash: "Welcome back!",
+    failureRedirect: "/login"
+    // failureFlash: "Incorrect login credentials."
+}), function(req,res){
+    //do nothing for now
+});
 app.get("/register", function(req,res){
     res.render("register");
+});
+app.post("/register", function(req,res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            // req.flash("error", err.message); TODO: add flash msgs once this works
+            res.redirect("back");
+            return;
+        }
+        passport.authenticate("local")(req,res,function(){
+            // req.flash("success", "Welcome to my blog "+user.username);
+            console.log("Success");
+            res.redirect("/");
+        });
+    });
 });
 //======= USER ROUTES =========
 app.get("/account/:id", function(req,res){
