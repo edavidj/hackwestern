d2a1294d05a8a644a1abc4531820cbc0b059295a
Ethan Johnston <johnse20@mcmaster.ca>
2017-11-18 02:15:04 -0500

    added routes and mongo connection

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-var express = require("express"),
-    app     = express();
+var express     = require("express"),
+    path        = require("path"),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    config      = require("./config"),
+    app         = express();
+
+mongoose.connect(config.db, {useMongoClient:true});
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true})); //parse form and query variables better
+app.set("view engine", "ejs");
+//for ref to front end files /<folder>/<file>
+//RENDER ROUTES
+app.get("/", function(req,res){
+    res.render("landing");
+});
+app.get("/login", function(req,res){
+    //login page here
+});
+app.get("/register", function(req,res){
+    //register here
+});
+app.get("/account/:id", function(req,res){
+    //render user page here
+});
+
+app.listen(3000, function(err){
+    if(err) throw err;
+    console.log("Connected to server.");    
+});
---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db: "mongodb://chelseaXu7:hackwestern4@ds035503.mlab.com:35503/mongohackwestern4"
+}
